cead480e4af125402aba23531cdfcfa92ba6e3b6
GitX123 <GitX123@github.com>
2023-06-24 12:48:14 +0800

    add two_sum.py

---
+++ array/two_sum.py
@@ -0,0 +1,21 @@
+from typing import List
+
+# Simple 
+class Solution(object):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if(i != j and nums[i] + nums[j] == target):
+                    return [i, j]
+        return []
+    
+# Improved (memory for speed)
+class Solution(object):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numToIndex = {}
+        for i in range(len(nums)):
+            if target - nums[i] in numToIndex:
+                return [numToIndex[target - nums[i]], i]
+            numToIndex[nums[i]] = i
+
+        return []
