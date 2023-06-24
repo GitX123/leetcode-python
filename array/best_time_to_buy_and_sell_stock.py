921938e642d5a87b2e31f27a79a285dadb08eb18
GitX123 <GitX123@github.com>
2023-06-24 14:28:51 +0800

    add best_time_to_buy_and_sell_stock.py

---
+++ array/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution(object):
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        price_min = float('inf')
+        for i in range(len(prices)):
+            if prices[i] < price_min:
+                price_min = prices[i]
+            else:
+                max_profit = max(max_profit, prices[i] - price_min)
+        return max_profit
