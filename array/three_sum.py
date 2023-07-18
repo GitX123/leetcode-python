d6d0430a1ad8e1569c743f8c70793b90c78dd888
GitX123 <GitX123@github.com>
2023-07-19 01:59:30 +0800

    try less effective(close to bruteforce) method to three sum

--- array/three_sum.py
+++ array/three_sum.py
@@ -21,6 +21,36 @@ class Solution(object):
             if i > 0 and a == nums[i-1]:
                 continue
 
+            # loop through b to find c
+            for j, b in enumerate(nums[i+1:]):
+                if a + b > 0:
+                    break
+
+                # pass duplicates
+                if j > 0 and b == nums[j+i]:
+                    continue
+
+                c = -a - b
+                if c in nums[j+i+2:]: # the in operator iterate over every element, which is slow in this case
+                    result.append([a, b, c])
+
+        return result
+    
+    def threeSum2(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        # sort input array
+        nums.sort()
+
+        # loop through a to find b, c
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+
+            # pass duplicates
+            if i > 0 and a == nums[i-1]:
+                continue
+
 
             # use left/right pointers to find b, c
             l, r = i + 1, len(nums) - 1
