6b87e249a4f37b425adb38d3e18f814597104bc8
jam3z <jam3z@example.com>
2026-05-31 17:00:48 +0900

    Add Tree: construct_binary_tree_from_preorder_inorder.py

--- tree/construct_binary_tree_from_preorder_inorder.py
+++ tree/construct_binary_tree_from_preorder_inorder.py
@@ -1,5 +1,10 @@
 """
+LeetCode 105
 
+Tips:
+* The first node of the preorder list is the root node.
+* The inorder will always go to the left, so the node from preorder corresponding to the node
+  in inorder that splits the tree to the left and right subtree.
 """
 
 
@@ -11,6 +16,25 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
+"""
+Recursive DFS
+
+Time Complexity: O(n^2)
+Space Complexity: O(n)
+"""
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        pass
+        if not preorder or inorder:
+            return None
+        
+        # first node of preorder is the root
+        root = TreeNode(preorder[0])
+
+        # find the position correspoding to the inorder
+        mid = inorder.index(preorder[0])
+
+        # build left and right subtree from the root nodes
+        root.left = self.buildTree(preorder[1: mid + 1], inorder[: mid])   # left subtree
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])     # right subtree
+        return root
