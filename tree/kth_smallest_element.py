9721664e5e83c08115cd068b469c9ed9e3732d3d
jam3z <jam3z@example.com>
2026-05-31 16:29:48 +0900

    Add kth_smallest_element.py in Tree

--- tree/kth_smallest_element.py
+++ tree/kth_smallest_element.py
@@ -1,3 +1,11 @@
+"""
+LeetCode 230
+
+Tips:
+* Traverse the tree with inorder traversal using DFS
+
+"""
+
 from typing import Optional
 
 class TreeNode:
@@ -8,4 +16,21 @@ class TreeNode:
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        pass        
+        n = 0 # tree pointer
+        stack = []
+
+        node = root
+        while node or stack:
+            # to left (dfs)
+            while node:
+                stack.append(node)
+                node = node.left
+
+            # pop if null
+            node = stack.pop()
+            n += 1
+            if n == k:
+                return node.val
+
+            # to right
+            node = node.right  
